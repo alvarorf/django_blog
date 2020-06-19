7775d7ec5371b4bc07c7b85ca30c3ff1bf216c9f
alvaro <aaruizf@unal.edu.co>
2020-06-19 16:49:08 -0400

    Added and migrated database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2020',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2020',
-    }
-
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
