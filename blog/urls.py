8c483dd8c095f5acbe255e11860e27e2189c1858
alvaro <aaruizf@unal.edu.co>
2020-06-23 21:50:16 -0400

    Added user password reset feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import (
     PostListView,
     PostDetailView,
@@ -19,4 +20,5 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    
 ]
