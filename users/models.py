ddc105e20a3d14c30dc035021197b4231b5fab51
alvaro <aaruizf@unal.edu.co>
2020-07-01 19:05:04 -0400

    Made some changes to deploy to heroku

--- users/models.py
+++ users/models.py
@@ -5,11 +5,12 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, **kwarhs):
-        super().save()
+    def save(self, *args, **kawrgs):
+        super().save(*args, **kawrgs)
 
         img = Image.open(self.image.path)
         
