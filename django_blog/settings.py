d5015d686f770e937b4e94deb8b5151b96ac63d6
alvaro <aaruizf@unal.edu.co>
2020-07-02 17:34:24 -0400

    Modified settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1', 'my-awesome-django-app-sample.herokuapp.com']
 
